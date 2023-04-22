34c557a60b8fd542d233720f23cbd6a2b95612f3
greyluo <g1luo@ucsd.edu>
2023-04-22 20:24:58 +0000

    config

--- express-app/app.js
+++ express-app/app.js
@@ -1,84 +1,82 @@
 const express = require('express');
-const app = express();
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'your_password',
   database: 'projects',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect();
-
-
 app.post('/api/create_project', (req, res) => {
-  const data = req.body;
-  const { title, description, catagories, tech_stack } = data;
+  const { title, description, categories, tech_stack } = req.body;
 
-  if (!title || !description || !catagories || !tech_stack) {
+  if (!title || !description || !categories || !tech_stack) {
     return res.json({ status: 'error', message: 'Please fill out all fields' });
   }
 
-  const insert_query = "INSERT INTO projects (title, description, catagories, tech_stack, state) VALUES (?, ?, ?, ? , 'open')";
-  db.query(insert_query, [title, description, catagories, tech_stack], (err, result) => {
+  const insertQuery = "INSERT INTO projects (title, description, categories, tech_stack, state) VALUES (?, ?, ?, ?, 'open')";
+  pool.query(insertQuery, [title, description, categories, tech_stack], (err, result) => {
     if (err) throw err;
     res.json({ status: 'success' });
   });
 });
 
 app.post('/api/create_user', (req, res) => {
-  const data = req.body;
-  const { username, email, password } = data;
+  const { username, email, password } = req.body;
 
-  const insert_query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-  db.query(insert_query, [username, email, password], (err, result) => {
+  const insertQuery = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+  pool.query(insertQuery, [username, email, password], (err, result) => {
     if (err) throw err;
     res.json({ status: 'success' });
   });
 });
 
 app.get('/api/get_projects', (req, res) => {
-  db.query("SELECT * FROM projects", (err, projects) => {
+  pool.query("SELECT * FROM projects", (err, projects) => {
     if (err) throw err;
     res.json({ status: 'success', projects: projects });
   });
 });
 
-app.post('/api/update-profile', (req, res) => {
-  const data = req.body;
-  const { username, skills, bio } = data;
+app.post('/api/update_profile', (req, res) => {
+  const { username, skills, bio } = req.body;
 
   if (!username || !skills || !bio) {
     return res.json({ status: 'error', message: 'Please fill out all fields' });
   }
 
-  const update_query = "UPDATE users SET username = ?, skills = ?, bio = ? WHERE username = ?";
-  db.query(update_query, [username, skills, bio, username], (err, result) => {
+  const updateQuery = "UPDATE users SET username = ?, skills = ?, bio = ? WHERE username = ?";
+  pool.query(updateQuery, [username, skills, bio, username], (err, result) => {
     if (err) throw err;
     res.json({ status: 'success' });
   });
 });
 
-app.post('/api/update-project', (req, res) => {
-  const data = req.body;
-  const { projectName, projectDescription, projectType, tech_stack, state } = data;
+app.post('/api/update_project', (req, res) => {
+  const { projectName, projectDescription, projectCategories, tech_stack, state } = req.body;
 
-  if (!projectName || !projectDescription || !projectType || !state) {
+  if (!projectName || !projectDescription || !projectCategories || !tech_stack || !state) {
     return res.json({ status: 'error', message: 'Please fill out all fields' });
   }
 
-  const update_query = "UPDATE projects SET title = ?, description = ?, catagories = ?, tech_stack = ?, state = ? WHERE title = ?";
-  db.query(update_query, [projectName, projectDescription, projectType, tech_stack, state, projectName], (err, result) => {
+  const updateQuery = "UPDATE projects SET title = ?, description = ?, categories = ?, tech_stack = ?, state = ? WHERE title = ?";
+  pool.query(updateQuery, [projectName, projectDescription, projectCategories, tech_stack, state, projectName], (err, result) => {
     if (err) throw err;
     res.json({ status: 'success' });
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
