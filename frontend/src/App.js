07d98466b1f218563c3b7b92b097bb8354b052e6
greyluo <luo80480@gmail.com>
2023-04-22 02:45:48 -0700

    test profile switching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginButton from './LoginButton';
-import LogoutButton from './logoutButton';
 import Profile from './Profile';
+
 
 
 function App() {
   return (
     <div className="App">
-        <LoginButton></LoginButton>
-        <LogoutButton></LogoutButton>
         <Profile></Profile>
     </div>
 
--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./logoutButton";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -9,12 +11,19 @@ const Profile = () => {
   }
 
   return (
-    isAuthenticated && (
+    isAuthenticated ? (
       <div>
+        <LogoutButton></LogoutButton>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+
+
       </div>
+    ) : (
+        <div>
+            <LoginButton></LoginButton>
+        </div>
     )
   );
 };
